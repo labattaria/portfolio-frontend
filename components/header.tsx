9878f1c68b153432fefc551cf04eca0c64e2010c
labattaria <hallucinate322@gmail.com>
2025-09-04 15:55:06 +0300

    add responsive fixed header component with navigation links

---
+++ components/header.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+export default function Header() {
+  const links = [
+    { name: "About me", href: "#about" },
+    { name: "Technologies", href: "#technologies" },
+    { name: "Projects", href: "#projects" },
+    { name: "Contact", href: "#contact" },
+  ];
+
+  const [isTop, setIsTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsTop(window.scrollY === 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header
+      className={`fixed top-0 left-0 w-full bg-header text-surface z-50 transition-all duration-300 ${
+        isTop ? "" : "shadow-[0_2px_6px_rgba(0,0,0,0.35)]"
+      }`}
+    >
+      <div
+        className={`max-w-7xl mx-auto flex justify-between items-center transition-all duration-300 ${
+          isTop ? "px-6 py-6" : "px-3 py-5"
+        }`}
+      >
+        <Link
+          href="/"
+          className="text-xl font-bold font-montserrat cursor-pointer"
+        >
+          Niko
+        </Link>
+
+        <nav>
+          <ul className="flex space-x-8">
+            {links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.href}
+                  className="text-surface uppercase font-semibold hover:text-accent transition-colors duration-300 font-montserrat"
+                >
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
