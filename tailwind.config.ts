c6fe49cb2320d24adc2ff3b58f5611ee6f74b991
labattaria <hallucinate322@gmail.com>
2025-09-04 15:51:12 +0300

    add and configure project setup config files (eslint, next, postcss, tailwind, tsconfig)

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: 'var(--font-inter)',
+        montserrat: 'var(--font-montserrat)',
+      },
+      colors: {
+        surface: 'var(--color-surface)',
+        ink: 'var(--color-ink)',
+        heading: 'var(--color-heading)',
+        accent: 'var(--color-accent)',
+        accent2: 'var(--color-accent-2)',
+      },
+    },
+  },
+}
