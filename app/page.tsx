e22613bc2eb2ebf901a2af10a4680cc7bd9bee4a
labattaria <hallucinate322@gmail.com>
2025-09-07 23:50:13 +0300

    add global padding-top

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,19 @@
 import About from "@/components/about";
 import Footer from "@/components/footer";
 import Hero from "@/components/hero";
+import Projects from "@/components/projects";
 import Technologies from "@/components/technologies";
 
 export default function Home() {
   return (
-    <main>
+    <main className="pt-[76px]">
       <Hero />
 
       <About />
 
       <Technologies />
+
+      <Projects />
 
       <Footer />
     </main>
