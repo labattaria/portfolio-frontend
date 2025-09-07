0e7182635e343fb25ed691bf300d8f7d929e2f40
labattaria <hallucinate322@gmail.com>
2025-09-07 23:47:21 +0300

    add projects data file for for mapping and rendering in Projects section

---
+++ data/projects.ts
@@ -0,0 +1,154 @@
+export const projects = [
+  {
+    title: "NextLevel Food",
+    description:
+      "This basic application demonstrates a platform for foodies to share their favorite recipes with the world. It's a place to discover new dishes and to connect with other food lovers.",
+    image: "/projects/foodies.png",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Prisma",
+      "PostgreSQL",
+      "AWS S3",
+    ],
+    github: "https://github.com/labattaria/nextlevel-food",
+    demo: "https://nextlevel-food-amber.vercel.app/",
+  },
+  {
+    title: "NextNews",
+    description:
+      "Next News is a modern news application built with Next.js. It allows you to stay up to date with the latest news and explore an archive of news from previous years and months. With smooth navigation, fast loading, and a clean interface, reading the news becomes simple and enjoyable.",
+    image: "/projects/nextnews.png",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Prisma",
+      "PostgreSQL",
+    ],
+    github: "https://github.com/labattaria/next-news",
+    demo: "https://next-news-indol.vercel.app/",
+  },
+  {
+    title: "PostWave",
+    description:
+      "PostWave is a modern Next.js demo application that brings people together to share anything they love — from ideas and stories to photos and creations. Post your thoughts, discover inspiring content from others, and show your appreciation with a like. A friendly space to connect, express, and be inspired.",
+    image: "/projects/postwave.png",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Prisma",
+      "PostgreSQL",
+      "Cloudinary",
+    ],
+    github: "https://github.com/labattaria/postwave-next",
+    demo: "https://postwave-next.vercel.app/",
+  },
+  {
+    title: "Authentication-demo",
+    description:
+      "This is a basic application that demonstrates simple user authentication and registration with additional form field validation.",
+    image: "/projects/authenticationdemo.png",
+    technologies: [
+      "Next.js",
+      "React",
+      "TypeScript",
+      "Tailwind CSS",
+      "Prisma",
+      "PostgreSQL",
+      "Lucia",
+    ],
+    github: "https://github.com/labattaria/nextjs-authentication-demo",
+    demo: "https://nextjs-authentication-demo-astra.vercel.app/",
+  },
+  {
+    title: "PizzaDay",
+    description:
+      "This basic application demonstrates a pizza delivery service example, where users can select multiple types of pizzas and specify the desired quantity for each.",
+    image: "/projects/pizzaday.png",
+    technologies: [
+      "Vite",
+      "React",
+      "React Router",
+      "React Hook Form",
+      "Redux",
+      "MUI",
+      "SASS",
+    ],
+    github: "https://github.com/labattaria/pizza-day",
+    demo: "https://pizza-day-azure.vercel.app/",
+  },
+  {
+    title: "Feedback Poster",
+    description:
+      "A simple app to share posts and see feedback in real time. Note: Before using the app, please make sure to start the server. The server may automatically go to sleep after periods of inactivity. You can wake it up by visiting this link: https://feedback-poster-server.onrender.com/posts",
+    image: "/projects/feedbackposter.png",
+    technologies: ["Vite", "React", "TypeScript", "React Router", "SASS"],
+    github: "https://github.com/labattaria/feedback-poster-client",
+    demo: "https://feedback-poster-client.vercel.app/",
+  },
+  {
+    title: "ChatApp",
+    description:
+      "A real-time chat application built with React, GraphQL, and Apollo Client. It leverages the WebSocket protocol and GraphQL Subscriptions to provide instant, bidirectional communication between client and server. Note: Before using the app, please make sure to start the server. The server may automatically go to sleep after periods of inactivity. You can wake it up by visiting this link: https://chat-app-server-0qdt.onrender.com/graphql",
+    image: "/projects/chatapp.png",
+    technologies: [
+      "Vite",
+      "React",
+      "React Router",
+      "GraphQL",
+      "Apollo Client",
+      "JWT",
+    ],
+    github: "https://github.com/labattaria/chat-app-client",
+    demo: "https://labattaria.github.io/chat-app-client/",
+  },
+  {
+    title: "Job Board",
+    description:
+      "A demo platform where you can browse current job openings and even add new ones. The board features listings from two companies, making it easy to explore opportunities and test the posting functionality. Note: Before using the app, please make sure to start the server. The server may automatically go to sleep after periods of inactivity. You can wake it up by visiting this link: https://job-board-server-mq1m.onrender.com",
+    image: "/projects/jobboard.png",
+    technologies: [
+      "Vite",
+      "React",
+      "React Router",
+      "GraphQL",
+      "Apollo Client",
+      "JWT",
+    ],
+    github: "https://github.com/labattaria/job-board-client",
+    demo: "https://job-board-client-liora.vercel.app/",
+  },
+  {
+    title: "WebStudio",
+    description:
+      "Adaptive landing page for a web studio with a nice design. Adapted for mobile phone, tablet and desktop.",
+    image: "/projects/webstudio.png",
+    technologies: ["HTML", "SASS", "Mobile First", "Responsive Design", "BEM"],
+    github: "https://github.com/labattaria/hw-08-WebStudio",
+    demo: "https://labattaria.github.io/hw-08-WebStudio/",
+  },
+  {
+    title: "ImageFinder",
+    description:
+      "Image search application by query name with brief photo statistics. It is also possible to view images in slider mode.",
+    image: "/projects/imagefinder.png",
+    technologies: ["JavaScript", "Axios", "REST API", "Parcel"],
+    github: "https://github.com/labattaria/goit-js-12",
+    demo: "https://labattaria.github.io/goit-js-12/",
+  },
+  {
+    title: "CountryFinder",
+    description:
+      "An interactive app that lets you search for countries by name and instantly explore detailed information. For each country, you can view its capital city, population, and official languages.",
+    image: "/projects/countryfinder.png",
+    technologies: ["JavaScript", "Axios", "REST API", "Parcel"],
+    github: "https://github.com/labattaria/goit-js-11",
+    demo: "https://labattaria.github.io/goit-js-11/",
+  },
+];
